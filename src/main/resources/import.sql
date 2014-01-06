a0eaf50a79288cf327a96c8123bbef653f64a359
vguerrero <vguerrero.net@gmail.com>
2014-01-06 17:33:29 -0500

    commit maven profiles working BD hsqldb and sql Server

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO contact (EMAIL,FIRSTNAME,LASTNAME,TELEPHONE) VALUES ('email1@email.com','Jhon','Anderson','60650654');
+INSERT INTO contact (EMAIL,FIRSTNAME,LASTNAME,TELEPHONE) VALUES ('email1@email2.com','Jhon2','Anderson','60650654');
+INSERT INTO contact (EMAIL,FIRSTNAME,LASTNAME,TELEPHONE) VALUES ('email1@email3.com','diana','haston','60650654');
+INSERT INTO contact (EMAIL,FIRSTNAME,LASTNAME,TELEPHONE) VALUES ('email1@email4.com','Robinsson','Relfech','60650654');
